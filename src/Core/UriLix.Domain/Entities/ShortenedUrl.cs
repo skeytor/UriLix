4a342ff0bd6737dd6f34e97f6efc2e19dc741416
Rober Guerrero <tj.rober.guerrero.m@upds.net.bo>
2025-02-19 13:31:20 -0400

    add User, ShortenedUrl and ClickStatistic entities

---
+++ src/Core/UriLix.Domain/Entities/ShortenedUrl.cs
@@ -0,0 +1,13 @@
+﻿namespace UriLix.Domain.Entities;
+
+public class ShortenedUrl
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public User? User { get; set; } = default;
+    public string OriginalUrl { get; set; } = string.Empty;
+    public string ShortCode { get; set; } = string.Empty;
+    public string? Alias { get; set; } = string.Empty;
+    public DateTime CreateAt { get; set; }
+    public DateTime UpdateAt { get; set; }
+}
