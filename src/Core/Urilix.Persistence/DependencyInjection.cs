d271903a32f384d2daf57fccfc502dcf7ede76a4
Rober Guerrero <tj.rober.guerrero.m@upds.net.bo>
2025-02-20 15:15:44 -0400

    update docker compose yml to launch browser

---
+++ src/Core/Urilix.Persistence/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UriLix.Persistence.Abstractions;
+
+namespace UriLix.Persistence;
+
+public static class DependencyInjection
+{
+    private const string _sectionName = "Database";
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IAppDbContext>(options => options.GetRequiredService<AppDbContext>());
+        return services;
+    }
+    public static IServiceCollection AddDatabaseProvider(
+        this IServiceCollection services, 
+        IConfiguration configuration)
+        => services.AddSqlServer<AppDbContext>(configuration.GetConnectionString(_sectionName));
+}
