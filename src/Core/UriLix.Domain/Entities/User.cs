a53d38359f593e25510076a4447a1ec8604627c9
Rober Guerrero <rober.guerrero@ucb.edu.bo>
2025-03-23 14:09:35 -0400

    add Identity Package in the Domain Layer

--- src/Core/UriLix.Domain/Entities/User.cs
+++ src/Core/UriLix.Domain/Entities/User.cs
@@ -1,12 +1,11 @@
-﻿namespace UriLix.Domain.Entities;
+﻿using Microsoft.AspNetCore.Identity;
 
-public class User
+namespace UriLix.Domain.Entities;
+
+public class User : IdentityUser
 {
-    public Guid Id { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
     public DateTime CreateAt { get; set; }
     public DateTime UpdateAt { get; set; }
     public DateTime? LastLoginAt { get; set; }
